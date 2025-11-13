a45d0b34effb1e7e19cc3a8725a0df2fa49bdb76
Alejandro <alexymartiygisbert@gmail.com>
2025-11-13 11:05:27 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,8 +2,15 @@ const CACHE_NAME = "clash-of-timers-v1";
 const urlsToCache = [
   "/Clash-of-Timers/",
   "/Clash-of-Timers/index.html",
-  "/Clash-of-Timers/style.css",
-  "/Clash-of-Timers/app.js",
+  "/Clash-of-Timers/assets/css/main.css",
+  "/Clash-of-Timers/assets/css/noscript.css",
+  "/Clash-of-Timers/assets/css/fontawesome-all.min.css",
+  "/Clash-of-Timers/js/boreakpoints.js",
+  "/Clash-of-Timers/js/browser.js",
+  "/Clash-of-Timers/js/jquery.js",
+  "/Clash-of-Timers/js/main.js",
+  "/Clash-of-Timers/js/script.js",
+  "/Clash-of-Timers/js/util.js",
   "/Clash-of-Timers/manifest.json"
 ];
 
